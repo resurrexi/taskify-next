f41bfc76c8f482793d60b47511c502e8d3a76eb3
Liquan Yang <yang.liquan87@gmail.com>
2023-12-29 13:39:31 -0500

    add skeleton for sidebar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -4,6 +4,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
 import { cn } from "@/lib/utils"
 import { Activity, CreditCard, Layout, Settings } from "lucide-react"
 import Image from "next/image"
@@ -97,5 +98,16 @@ export function NavItem({
         ))}
       </AccordionContent>
     </AccordionItem>
+  )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="relative h-10 w-10 shrink-0">
+        <Skeleton className="absolute h-full w-full" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   )
 }
