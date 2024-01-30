c93d0028ba3121d4f98d8d3dceb2de6147217056
Liquan Yang <yang.liquan87@gmail.com>
2024-01-29 22:19:40 -0500

    implement delete board in board page

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,4 +1,5 @@
 import { Board } from "@prisma/client"
+import { BoardOptions } from "./board-options"
 import { BoardTitleForm } from "./board-title-form"
 
 interface BoardNavbarProps {
@@ -9,6 +10,9 @@ export const BoardNavbar = ({ data }: BoardNavbarProps) => {
   return (
     <div className="fixed top-14 z-[40] flex h-14 w-full items-center gap-x-4 bg-black/50 px-6 text-white">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   )
 }
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,18 +1,32 @@
 "use client"
 
+import { updateBoard } from "@/actions/update-board"
 import { FormInput } from "@/components/form/form-input"
 import { Button } from "@/components/ui/button"
+import { useAction } from "@/hooks/use-action"
 import { Board } from "@prisma/client"
 import { ElementRef, useRef, useState } from "react"
+import { toast } from "sonner"
 
 interface BoardTitleFormProps {
   data: Board
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated!`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
   const formRef = useRef<ElementRef<"form">>(null)
   const inputRef = useRef<ElementRef<"input">>(null)
 
+  const [title, setTitle] = useState(data.title)
   const [isEditing, setIsEditing] = useState(false)
 
   const enableEditing = () => {
@@ -27,7 +41,11 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string
-    console.log("submitted", title)
+
+    execute({
+      title,
+      id: data.id,
+    })
   }
 
   const onBlur = () => {
@@ -45,7 +63,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
           ref={inputRef}
           id="title"
           onBlur={onBlur}
-          defaultValue={data.title}
+          defaultValue={title}
           className="h-7 border-none bg-transparent px-[7px] py-1 text-lg font-bold focus-visible:outline-none focus-visible:ring-transparent"
         />
       </form>
@@ -58,7 +76,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       variant="transparent"
       className="h-auto w-auto p-1 px-2 text-lg font-bold"
     >
-      {data.title}
+      {title}
     </Button>
   )
 }
