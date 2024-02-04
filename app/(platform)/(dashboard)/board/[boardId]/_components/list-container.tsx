6712bd5cd1ddd0d29783c8b6478b42bf3819baa2
Liquan Yang <yang.liquan87@gmail.com>
2024-02-04 18:47:48 -0500

    add list update behavior and list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { ListWithCards } from "@/types"
+import { useEffect, useState } from "react"
 import { ListForm } from "./list-form"
+import { ListItem } from "./list-item"
 
 interface ListContainerProps {
   data: ListWithCards[]
@@ -9,8 +11,17 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data)
+
+  useEffect(() => {
+    setOrderedData(data)
+  }, [data])
+
   return (
-    <ol>
+    <ol className="flex h-full gap-x-3">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />
+      })}
       <ListForm />
       <div className="w-1 flex-shrink-0" />
     </ol>
