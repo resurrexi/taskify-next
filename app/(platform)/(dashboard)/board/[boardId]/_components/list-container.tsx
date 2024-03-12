840a1bff6ce2cfb33fd3e8376d54e463832d03ce
Liquan Yang <yang.liquan87@gmail.com>
2024-03-11 22:38:41 -0400

    wip: add drag-n-drop behavior

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ListWithCards } from "@/types"
+import { DragDropContext, Droppable } from "@hello-pangea/dnd"
 import { useEffect, useState } from "react"
 import { ListForm } from "./list-form"
 import { ListItem } from "./list-item"
@@ -18,12 +19,23 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data])
 
   return (
-    <ol className="flex h-full gap-x-3">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} index={index} data={list} />
-      })}
-      <ListForm />
-      <div className="w-1 flex-shrink-0" />
-    </ol>
+    <DragDropContext onDragEnd={() => { }}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex h-full gap-x-3"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="w-1 flex-shrink-0" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
