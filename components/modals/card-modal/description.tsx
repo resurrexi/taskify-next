8dfb303ff415cfaa16e308f821b9e9ec3fe23e4c
Liquan Yang <yang.liquan87@gmail.com>
2024-03-17 23:01:38 -0400

    wip: persist card description changes

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,14 +1,17 @@
 "use client"
 
+import { updateCard } from "@/actions/update-card"
 import { FormSubmit } from "@/components/form/form-submit"
 import { FormTextarea } from "@/components/form/form-textarea"
 import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
+import { useAction } from "@/hooks/use-action"
 import { CardWithList } from "@/types"
 import { useQueryClient } from "@tanstack/react-query"
 import { AlignLeft } from "lucide-react"
 import { useParams } from "next/navigation"
 import { ElementRef, useRef, useState } from "react"
+import { toast } from "sonner"
 import { useEventListener, useOnClickOutside } from "usehooks-ts"
 
 interface DescriptionProps {
@@ -44,11 +47,28 @@ export const Description = ({ data }: DescriptionProps) => {
   useEventListener("keydown", onKeyDown)
   useOnClickOutside(formRef, disableEditing)
 
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      })
+      toast.success(`Card "${data.title}" updated`)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
   const onSubmit = (formData: FormData) => {
     const description = formData.get("description") as string
     const boardId = params.boardId as string
 
-    // TODO: execute
+    execute({
+      description,
+      boardId,
+      id: data.id,
+    })
   }
 
   return (
@@ -57,12 +77,14 @@ export const Description = ({ data }: DescriptionProps) => {
       <div className="w-full">
         <p className="font-neutral-700 mb-2 font-semibold">Description</p>
         {isEditing ? (
-          <form ref={formRef} className="space-y-2">
+          <form action={onSubmit} ref={formRef} className="space-y-2">
             <FormTextarea
+              ref={textareaRef}
               id="description"
               className="mt-2 w-full"
               placeholder="Add a more detailed description"
               defaultValue={data.description || undefined}
+              errors={fieldErrors}
             />
             <div className="flex items-center gap-x-2">
               <FormSubmit>Save</FormSubmit>
@@ -86,7 +108,6 @@ export const Description = ({ data }: DescriptionProps) => {
           </div>
         )}
       </div>
-      {data.description}
     </div>
   )
 }
