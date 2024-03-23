d30b8a5a6c59cb9dcd3d24a269aae8cada9beaf9
Liquan Yang <yang.liquan87@gmail.com>
2024-03-23 16:37:23 -0400

    wip: add stripe webhook + unlock unlimited board

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -4,6 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 import { MAX_FREE_BOARDS } from "@/constants/boards"
 import { db } from "@/lib/db"
 import { getAvailableCount } from "@/lib/org-limit"
+import { checkSubscription } from "@/lib/subscription"
 import { auth } from "@clerk/nextjs"
 import { HelpCircle, User2 } from "lucide-react"
 import Link from "next/link"
@@ -26,6 +27,7 @@ export async function BoardList() {
   })
 
   const availableCount = await getAvailableCount()
+  const isPro = await checkSubscription()
 
   return (
     <div className="space-y-4">
@@ -51,9 +53,11 @@ export async function BoardList() {
             className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">{`${
-              MAX_FREE_BOARDS - availableCount
-            } remaining`}</span>
+            <span className="text-xs">
+              {isPro
+                ? "Unlimited"
+                : `${MAX_FREE_BOARDS - availableCount} remaining`}
+            </span>
             <Hint
               sideOffset={40}
               description="Free workspaces can have up to 5 open boards. For unlimited boards, upgrade this workspace."
